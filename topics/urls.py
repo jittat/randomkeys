d7b3754724b0bcd2edc037a9e58e3da54e6e818b
Jittat Fakcharoenphol <jittat@gmail.com>
2015-08-28 23:35:46 +0700

    add new keywords

--- topics/urls.py
+++ topics/urls.py
@@ -3,6 +3,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^([^\/]+)/$', views.index, name='topics-index'),
-    url(r'^([^\/]+)/keywords/$', views.list, name='topics-keywords'),
+    url(r'^([^\/]+)/$',
+        views.index,
+        name='topics-index'),
+    
+    url(r'^([^\/]+)/keywords/$',
+        views.list,
+        name='topics-keywords'),
+    
+    url(r'^([^\/]+)/keywords/add/$',
+        views.add_keyword,
+        name='topics-add-keywords'),
 ]
--- topics/views.py
+++ topics/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 import json
 
-from models import Room
+from models import Room, Keyword
 
 def index(request, room_title):
     room, is_created = Room.objects.get_or_create(title=room_title)
@@ -23,5 +23,15 @@ def list(request, room_title):
     json_data = json.dumps(names)
     
     return HttpResponse(json_data, content_type='application/json')
-    
+
+
+def add_keyword(request, room_title):
+    room = get_object_or_404(Room, title=room_title)
+
+    keyword_str = request.POST['keyword'].strip()
+    keywords = room.keyword_set.filter(name=keyword_str).all()
+    if len(keywords)==0:
+        keyword = Keyword(name=keyword_str, room=room)
+        keyword.save()
+    return HttpResponse('OK')
 
